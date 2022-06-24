219ec47d5bf5f0a591ac12c0488fd21a8162ce14
MuhammadZubair786 <mzhassan444@gmail.com>
2022-06-24 21:42:09 +0500

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:dynamiclink/Firebasedynamic.dart';
 import 'package:dynamiclink/Home.dart';
 import 'package:dynamiclink/Loginuser.dart';
 import 'package:dynamiclink/PageShow.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -33,57 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String url = "";
-
-  @override
-  void initState() {
-    initDynamicLinks();
-    super.initState();
-  }
-
-  ///Retreive dynamic link firebase.
-  void initDynamicLinks() async {
-    print("ok");
-    final PendingDynamicLinkData? data =
-        await FirebaseDynamicLinks.instance.getInitialLink();
-    final Uri? deepLink = data?.link;
-    print("Link +" + deepLink.toString());
-
-    if (deepLink != null) {
-      handleDynamicLink(deepLink);
-    }
-    FirebaseDynamicLinks.instance.onLink.listen((event) {
-      print("Link event :" + event.toString());
-      final Uri? deepLink = event.link;
-
-      // var data = event as Map;
-      print(deepLink);
-      handleDynamicLink(deepLink!);
-    });
-  }
-
-  handleDynamicLink(Uri url) {
-    print(url);
-    List<String> separatedString = [];
-    separatedString.addAll(url.path.split('?'));
-
-    var isStory = url.pathSegments.contains('user');
-    print(separatedString);
-    print(isStory);
-
-    if (isStory) {
-      var id = url.queryParameters["id"];
-      print(id);
-    }
-
-    Timer(
-        Duration(seconds: 5),
-        () => {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => HomeApp()))
-            });
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
